2be97b9c3ed567be8bdad85c6f8ae63aa427717e
abelonogov <acebel@mail.ru>
2015-02-18 10:24:18 +0200

    Workinkg with template.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  def profile
+  def index
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/index'
+
   devise_for :users, controllers: { registrations: 'registrations' }
 
-  root 'users#profile'
+  root 'sessions#index'
 
   # resources :users
 
