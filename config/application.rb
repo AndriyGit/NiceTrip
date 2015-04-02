9e135cb57ed1590a205bb3fa7d01e99c07f1e0ad
abelonogov <acebel@mail.ru>
2015-04-03 00:14:45 +0300

    Fix for heroku.

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ Bundler.require(*Rails.groups)
 module NiceTrip
   class Application < Rails::Application
     config.mongoid.logger = Logger.new($stdout, :warn)
+    config.assets.initialize_on_precompile=false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
